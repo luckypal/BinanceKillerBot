9e061d6c6bd1e129bb3d7877393027ca180c12ef
luckypal <bigluckypal@gmail.com>
2021-10-08 23:07:17 -0500

    Update ci/cd for cstar

--- pm2/reload-pm2.sh
+++ pm2/reload-pm2.sh
@@ -10,8 +10,24 @@ npm run build
 
 pm2 delete Binance-Killer-Bot
 
-pm2 delete Binance-Killer-Bot-cstar
-
 pm2 start pm2/start.json
 
+
+
+
+cd ~/trade/bk-bot-cstar
+
+npm install
+
+# curl http://127.0.0.1/save
+
+npm run build
+
+pm2 delete Binance-Killer-Bot-cstar
+
 pm2 start pm2/start-cstar.json
+
+
+
+# pm2 delete Binance-Killer-Bot-cstar
+# pm2 start pm2/start-cstar.json
